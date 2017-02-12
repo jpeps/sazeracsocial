116f957f494098c967cf75829f9023639e7a910c
jeldridgeTS <jeldridge@turbosquid.com>
2017-02-12 15:05:19 -0600

    add lots of dependencies

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 3000))
+
+// Public Dir
+app.use(express.static(__dirname + '/public'))
+
+// Express Configs
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  res.render('landing')
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+})
