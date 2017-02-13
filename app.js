810db603229d148137bdb54dcf8b0ca61c85a14f
jeldridgeTS <jeldridge@turbosquid.com>
2017-02-13 09:44:09 -0600

    Add temp static pages for quick posting Add bootstrap and font-awesome to package.json Include and link up bootstrap, jquery, and font-awesome

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ app.set('port', (process.env.PORT || 3000))
 
 // Public Dir
 app.use(express.static(__dirname + '/public'))
+app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+app.use('/js',  express.static(__dirname + '/node_modules/jquery/dist'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/fa', express.static(__dirname + '/node_modules/font-awesome'))
 
 // Express Configs
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-  res.render('landing')
+  res.render('temp_static_pages/landingtest')
 })
 
 app.listen(app.get('port'), function() {
