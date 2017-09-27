bec997fd8108576af4e3dcee6061efe740a24043
James Eldridge <james.eldridge@mail.com>
2017-09-27 22:08:13 +0000

    Replace old landing page and static pages, cleanup app.js, TODO: remove node modules for bootstrap/jquery

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ var express               = require('express'),
 app.set('port', (process.env.PORT || 3000))
 
 // Public Dir
-app.use(express.static(__dirname + '/public'))
+app.use(        express.static(__dirname + '/public'))
 app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js'))
 app.use('/js',  express.static(__dirname + '/node_modules/jquery/dist'))
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-app.use('/fa', express.static(__dirname + '/node_modules/font-awesome'))
+app.use('/fa',  express.static(__dirname + '/node_modules/font-awesome'))
 
 // Express Configs
 app.set('view engine', 'ejs')
@@ -47,16 +47,16 @@ app.use(function (req, res, next) {
 
 // Routes
 var register_routes = require('./routes/register.routes')
-var login_routes = require('./routes/login.routes')
-var post_routes = require('./routes/posts.routes')
-var user_routes = require('./routes/users.routes')
-var secret_routes = require('./routes/superSecret.routes')
+var login_routes    = require('./routes/login.routes')
+var post_routes     = require('./routes/posts.routes')
+var user_routes     = require('./routes/users.routes')
+var secret_routes   = require('./routes/superSecret.routes')
 
 app.use('/register', register_routes)
-app.use('/login', login_routes)
-app.use('/posts', post_routes)
-app.use('/users', user_routes)
-app.use('/secret', secret_routes)
+app.use('/login',    login_routes)
+app.use('/posts',    post_routes)
+app.use('/users',    user_routes)
+app.use('/secret',   secret_routes)
 
 app.get('/logout', function(req, res) {
     req.logout()
@@ -66,25 +66,7 @@ app.get('/logout', function(req, res) {
 // ---------- TEMP ROUTING ---------- //
 
 app.get('/', function(req, res) {
-    res.render('temp_static_pages/landingtest')
-})
-
-app.get('/about', function(req, res) {
-    res.render('temp_static_pages/about')
-})
-
-app.get('/contact', function(req, res) {
-    res.render('temp_static_pages/contact')
-})
-
-app.get('/post', function(req, res) {
-  res.render('temp_static_pages/post')
-})
-
-// Admin routes
-
-app.get('/admin', function(req, res) {
-    res.render('temp_static_pages/admin')
+    res.render('static_pages/landing')
 })
 
 // ---------- TEMP ROUTING ---------- //
